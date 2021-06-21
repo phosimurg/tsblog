a6c246bd526b51d6af5904b3fb07d1b0a2756011
Ahmet YILDIZ <oyunyapimcisi123@gmail.com>
2021-06-21 16:20:05 +0300

    init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
+
+// add bootstrap css
+import 'bootstrap/dist/css/bootstrap.css'
+//import "../css/customcss.css";
+
+
+
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
